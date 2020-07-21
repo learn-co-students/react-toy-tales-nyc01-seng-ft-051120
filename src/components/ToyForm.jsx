2256d666c2faabe14be908c5808f4cc8185d4d5c
kevinneyer <kevinneyer@gmail.com>
2020-07-20 21:05:03 -0400

    Done.

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 class ToyForm extends Component {
 
   render() {
+
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form onSubmit={this.props.submitHandler} className="add-toy-form">
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input onChange={this.props.handleForm} value={this.props.name} type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input onChange={this.props.handleForm} value={this.props.image} type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
