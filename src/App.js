2d9081437ef9ed7fd84407ac61d263f74ccf2746
ChayaGreisman <chayamgreisman@gmail.com>
2020-07-21 16:19:25 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import data from './data'
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toys: []
   }
 
   handleClick = () => {
@@ -21,20 +22,73 @@ class App extends React.Component{
     })
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({toys: data})
+    })
+  }
+
+  handleNewToy = (newToy) => {
+    this.state( {toys: [...this.state.toys, newToy]} )
+  }
+
+  deleteToy = toyId => {
+    fetch(`http://localhost:3000/toys/${toyId}`, {
+      method: 'DELETE',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(() => {
+      let newToys = this.state.toys.filter(toy => toy.id !== toyId)
+      this.setState({toys: newToys})
+    })
+  }
+
+  likeToy = (toyId, toyLikes) => {
+    fetch(`http://localhost:3000/toys/${toyId}`, {
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }, 
+      body: JSON.stringify( {
+        likes: toyLikes + 1
+      } )
+    })
+    .then(res => res.json())
+    .then(updatedToy => {
+      let newToyArray = this.state.toys.map(toy => {
+        if (toy.id === toyId) {
+          return updatedToy
+        }
+        return toy
+      }
+      )
+      this.setState({toys: newToyArray})
+    })
+
+  }
+
   render(){
+    console.log(this.state)
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm handleNewToy={this.handleNewToy}/>
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toys={this.state.toys} deleteToy={this.deleteToy} likeToy={this.likeToy}/>
       </>
     );
   }
--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ToyCard from './ToyCard'
 
-const ToyContainer = () => {
+const ToyContainer = (props) => {
   return(
     <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
+      {props.toys.map(toy => <ToyCard key={toy.id} {...toy} deleteToy={props.deleteToy} likeToy={props.likeToy}/>)}
     </div>
   );
 }
-
 export default ToyContainer;
