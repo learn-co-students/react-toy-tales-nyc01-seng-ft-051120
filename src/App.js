0521b4bb4dbabcf3efea367598412e487579a9ed
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-07-20 21:06:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,38 @@ import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
-import data from './data'
-
-
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toys: []
+  }
+
+  componentDidMount(){
+      this.fetchToys()
+  }
+
+  fetchToys = () => {
+    fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then(data => {
+        this.setState({ toys: data})
+    })
+  }
+  postToy(e) {
+    fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+       'Content-Type': 'application/json',
+       Accept: 'application/json'
+    },
+    body: JSON.stringify({
+        name: e.name,
+        image: e.image,
+        likes: 0
+     })
+   })
+
   }
 
   handleClick = () => {
@@ -22,19 +47,20 @@ class App extends React.Component{
   }
 
   render(){
+    console.log(this.state)
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm postToy={this.postToy}/>
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toys={this.state.toys} fetch={this.fetchToys}/>
       </>
     );
   }
